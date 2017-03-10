20683bf0e88f35dc7c1745273900558da1498dc0
Nicolas Vanheuverzwijn <nicolas.vanheu@gmail.com>
2017-03-10 17:43:30 -0500

    feat: ajout d'un quatrieme argument pour le amazon id

--- docker/aws-push.sh
+++ docker/aws-push.sh
@@ -3,12 +3,13 @@
 image=$1
 region=$2
 tag=$3
+aws_id=$4
 
 # Backtick (`) are required for the login session to be kept alive throughout the execution of this script
 `aws ecr get-login --region $region` > /dev/null 2>&1
-docker tag $image:$tag 611542441284.dkr.ecr.$region.amazonaws.com/$image:$tag
+docker tag $image:$tag $aws_id.dkr.ecr.$region.amazonaws.com/$image:$tag
 aws ecr describe-repositories --region $region --repository-name $image > /dev/null 2>&1
 if [ $? -ne 0 ]; then
   aws ecr create-repository --region $region --repository-name $image
 fi
-docker push 611542441284.dkr.ecr.$region.amazonaws.com/$image:$tag
+docker push $aws_id.dkr.ecr.$region.amazonaws.com/$image:$tag
