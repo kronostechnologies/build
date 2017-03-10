6e24d73880ef56434ae1133365250d1771bb7ec1
Nicolas Vanheuverzwijn <nicolas.vanheu@gmail.com>
2017-03-10 16:38:36 -0500

    feat: add thrid argument for tag

--- docker/aws-push.sh
+++ docker/aws-push.sh
@@ -2,12 +2,13 @@
 
 image=$1
 region=$2
+tag=$3
 
-# Backtick (`) are required for the login session to be kept alive thoughout the execution of this script
+# Backtick (`) are required for the login session to be kept alive throughout the execution of this script
 `aws ecr get-login --region $region` > /dev/null 2>&1
-docker tag $image:latest 611542441284.dkr.ecr.$region.amazonaws.com/$image:latest
+docker tag $image:$tag 611542441284.dkr.ecr.$region.amazonaws.com/$image:$tag
 aws ecr describe-repositories --region $region --repository-name $image > /dev/null 2>&1
 if [ $? -ne 0 ]; then
   aws ecr create-repository --region $region --repository-name $image
 fi
-docker push 611542441284.dkr.ecr.$region.amazonaws.com/$image:latest
+docker push 611542441284.dkr.ecr.$region.amazonaws.com/$image:$tag
