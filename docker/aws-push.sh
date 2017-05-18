87fe3e1f5a59b65ec9c02fddaef17fb5f3bf4b1f
Antoine Deschênes <adeschenes@kronostechnologies.com>
2017-05-18 08:47:56 -0400

    fix(aws-push): Source image is not tagged

--- docker/aws-push.sh
+++ docker/aws-push.sh
@@ -7,7 +7,7 @@ aws_id=$4
 
 # Backtick (`) are required for the login session to be kept alive throughout the execution of this script
 `aws ecr get-login --region $region` > /dev/null 2>&1
-docker tag $image:$tag $aws_id.dkr.ecr.$region.amazonaws.com/$image:$tag
+docker tag $image $aws_id.dkr.ecr.$region.amazonaws.com/$image:$tag
 aws ecr describe-repositories --region $region --repository-name $image > /dev/null 2>&1
 if [ $? -ne 0 ]; then
   aws ecr create-repository --region $region --repository-name $image
