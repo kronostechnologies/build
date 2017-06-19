5a21fbbc1e074206465960970ebc4b2d63d0b3ee
Antoine Deschênes <adeschenes@kronostechnologies.com>
2017-06-19 15:18:37 -0400

    kbuild install script supports user install

--- kbuild/install-linux.sh
+++ kbuild/install-linux.sh
@@ -3,7 +3,11 @@ set -e
 
 DOT="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
 
-if [ ! -h /usr/local/bin/kbuild ]
-then
-  ln -s $DOT/kbuild /usr/local/bin/kbuild
+if ! command -v kbuild >/dev/null 2>&1; then
+  if [ "`id -u`" -eq 0 ]; then
+    ln -nsf $DOT/kbuild /usr/local/bin/
+  else
+    mkdir -p ~/bin
+    ln -nsf $DOT/kbuild ~/bin/
+  fi
 fi
