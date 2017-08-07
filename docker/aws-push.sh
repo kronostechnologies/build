91b4959795532560f86f28697530dcd875416d92
Nicolas Vanheuverzwijn <nicolas.vanheu@gmail.com>
2017-08-07 14:11:02 -0400

    add --no-include-email option to get-login call

--- docker/aws-push.sh
+++ docker/aws-push.sh
@@ -6,7 +6,7 @@ tag=$3
 aws_id=$4
 
 # Backtick (`) are required for the login session to be kept alive throughout the execution of this script
-`aws ecr get-login --region $region` > /dev/null 2>&1
+`aws ecr get-login --region $region --no-include-email` > /dev/null 2>&1
 docker tag $image $aws_id.dkr.ecr.$region.amazonaws.com/$image:$tag
 aws ecr describe-repositories --region $region --repository-name $image > /dev/null 2>&1
 if [ $? -ne 0 ]; then
