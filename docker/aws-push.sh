3a5cba7316314d32274052293b611fec14c45582
Nicolas Vanheuverzwijn <nicolas.vanheu@gmail.com>
2017-08-08 12:24:27 -0400

    support old circleci docker version

--- docker/aws-push.sh
+++ docker/aws-push.sh
@@ -5,8 +5,14 @@ region=$2
 tag=$3
 aws_id=$4
 
+DOCKER_VERSION=`docker version -f '{{.Server.Version}}'`
+
 # Backtick (`) are required for the login session to be kept alive throughout the execution of this script
-`aws ecr get-login --region $region --no-include-email` > /dev/null 2>&1
+if [[ $DOCKER_VERSION = '1.9.1-circleci-cp-workaround' ]]; then
+  `aws ecr get-login --region $region` > /dev/null 2>&1
+else
+  `aws ecr get-login --region $region --no-include-email` > /dev/null 2>&1
+fi
 docker tag $image $aws_id.dkr.ecr.$region.amazonaws.com/$image:$tag
 aws ecr describe-repositories --region $region --repository-name $image > /dev/null 2>&1
 if [ $? -ne 0 ]; then
