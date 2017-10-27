07b632574d43faa395767433f6af885bec13e809
Nicolas Vanheuverzwijn <nicolas.vanheu@gmail.com>
2017-10-27 16:27:02 -0400

    remove docker version check for aws-push

--- docker/aws-push.sh
+++ docker/aws-push.sh
@@ -8,11 +8,7 @@ aws_id=$4
 DOCKER_VERSION=`docker version -f '{{.Server.Version}}'`
 
 # Backtick (`) are required for the login session to be kept alive throughout the execution of this script
-if [[ $DOCKER_VERSION = '1.9.1-circleci-cp-workaround' ]]; then
-  `aws ecr get-login --region $region` > /dev/null 2>&1
-else
-  `aws ecr get-login --region $region --no-include-email` > /dev/null 2>&1
-fi
+`aws ecr get-login --region $region` > /dev/null 2>&1
 docker tag $image $aws_id.dkr.ecr.$region.amazonaws.com/$image:$tag
 aws ecr describe-repositories --region $region --repository-name $image > /dev/null 2>&1
 if [ $? -ne 0 ]; then
